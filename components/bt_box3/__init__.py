e1302eb5aa1166f8ff2e20d0fa0e815ca8f5efdb
youkorr <96953496+youkorr@users.noreply.github.com>
2025-04-04 01:44:23 +0200

    Update __init__.py

--- components/bt_box3/__init__.py
+++ components/bt_box3/__init__.py
@@ -1,19 +1,20 @@
+"""ESP32-S3 Box 3 Bluetooth Audio component for ESPHome."""
+
 import esphome.codegen as cg
 import esphome.config_validation as cv
+from esphome.components import component
 from esphome.const import CONF_ID
 
 DEPENDENCIES = ["esp32"]
-AUTO_LOAD = ["media_player"]
+CODEOWNERS = ["@your_username"]
 
-host_box3_ns = cg.esphome_ns.namespace("host_box3")
-HostBox3Component = host_box3_ns.class_("HostBox3Component", cg.Component)
+CONF_BT_BOX3_ID = "bt_box3_id"
 
-CONFIG_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(): cv.declare_id(HostBox3Component),
-    }
-).extend(cv.COMPONENT_SCHEMA)
+bt_box3_ns = cg.esphome_ns.namespace("bt_box3")
+BTBox3Component = bt_box3_ns.class_("BTBox3Component", component.Component)
+
+CONFIG_SCHEMA = component.component_schema(BTBox3Component)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
+    await component.register_component(var, config)
